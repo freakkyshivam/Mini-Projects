0a296ad650eae0f48d8a455989d12069b702ac3f
Shivam Chaudhary <skc722768@gmail.com>
2025-12-27 21:51:40 +0530

    fix ts annotation

--- OTP based authentication system/backend/src/config/nodemailer.ts
+++ OTP based authentication system/backend/src/config/nodemailer.ts
@@ -1,10 +1,11 @@
-import nodemailer from 'nodemailer'
+import nodemailer  from 'nodemailer'
+import type { Transporter } from "nodemailer";
 
-export const transporter =   nodemailer.createTransport({
+const transporter: Transporter = nodemailer.createTransport({
         host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,  
-  auth: {
+      port: 587,
+    secure: false,  
+    auth: {
     user: process.env.SMTP_USER!,
     pass: process.env.SMTP_PASSWORD,
   },
--- OTP based authentication system/backend/src/config/redis.ts
+++ OTP based authentication system/backend/src/config/redis.ts
@@ -1,11 +1,11 @@
-import { createClient } from "redis";
+import { createClient, RedisClientType } from "redis";
 
-export const redis = createClient({
+export const redis:RedisClientType = createClient({
   url: process.env.REDIS_URL || "redis://localhost:6379",
 });
 
-redis.on("error", (err) => {
-  console.error("Redis Error:", err);
+redis.on("error", (err: Error) => {
+  console.error("Redis error:", err);
 });
 (async ()=>{
 await redis.connect();
